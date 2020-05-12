57a799404e6da90c761dd771804d99e81805a2a1
Romy Maghsoudi <romy_mag@outlook.com>
2020-05-12 10:14:15 -0400

    basic user fetch on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import EntryList from "./EntryList";
 // Temporary dummy data for testing
-import { dummyEntries, dummyUser } from "../constants/constants";
-import { apiCalls } from '../hooks/APICalls'
+import { dummyEntries } from "../constants/constants";
+import { ApiCalls } from "../hooks/APICalls";
 import UserLeveling from "./UserLeveling";
 
 const Dashboard = () => {
+  const { apiUser, getUser } = ApiCalls();
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="dashboard">
       <h1>Dashboard</h1>
-      <EntryList entries={dummyEntries} />
-      <UserLeveling user={dummyUser} />
+      {apiUser ? (
+        <>
+          {console.log(apiUser)}
+          <EntryList entries={dummyEntries} />
+          <UserLeveling user={apiUser} />
+        </>
+      ) : (
+        <p>We loadin fam</p>
+      )}
     </div>
   );
 };
--- src/hooks/APICalls.js
+++ src/hooks/APICalls.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import {apiUrl} from "../constants/constants";
 
-export function apiCalls() {
-  const {isLoading, setIsLoading} = useState(true);
-  const {apiUser, setApiUser} = useState();
+export function ApiCalls() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [apiUser, setApiUser] = useState(null);
 
   const getUser = async () => {
-    const user = await axios.get(`${apiUrl}/users`);
-    setApiUser(user);
-    setIsLoading(false);
+    const res = await axios.get("http://localhost:8000/users/")
+    setApiUser(res.data[0])
+    setIsLoading(false)
   };
 
   return {
