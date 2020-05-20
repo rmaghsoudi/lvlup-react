c679ddbd4d8447eb7e8c8b7ba0014bc629305d8e
Romy Maghsoudi <romy_mag@outlook.com>
2020-05-20 10:27:55 -0400

    added labels for modal

--- src/components/EntryModal.js
+++ src/components/EntryModal.js
@@ -1,16 +1,33 @@
 import React from "react";
 
 const EntryModal = (props) => {
-  const entry = props.entry; 
+  const entry = props.entry;
 
   return (
     <div className="entry-modal">
       <div className="entry-modal-content">
         <h3 contentEditable="true">{entry.name}</h3>
         <div className="card-content">
-          <p>Type {entry.type}</p>
-          <p>Difficulty <input type="number" min="1" max="10" value={entry.difficulty} /></p>
-          <p>Completed <input type="checkbox" checked={entry.completed} /></p>
+          
+          <label for="type-select">Type</label>
+          <select name="types" id="type-select" value={entry.type}>
+            <option value="task">Task</option>
+            <option value="pillar">Pillar</option>
+            <option value="goal">Goal</option>
+          </select>
+
+          <label for="difficulty">Difficulty</label>
+          <input
+            type="number"
+            name="difficulty"
+            min="1"
+            max="10"
+            value={entry.difficulty}
+          />
+
+          <label for="completed">Completed</label>
+          <input type="checkbox" name="completed" checked={entry.completed} />
+
           <button onClick={() => props.toggleModal()}>Close Me</button>
         </div>
       </div>
